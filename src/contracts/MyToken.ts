b94c1c7453e0a5fc37dd96e6da3da049d1de7b82
Extrabest <136263155+Extrabest@users.noreply.github.com>
2025-07-14 16:51:51 +0100

    Update MyToken.ts
    BEST

--- src/contracts/MyToken.ts
+++ src/contracts/MyToken.ts
@@ -21,10 +21,10 @@ export class MyToken extends DeployableOP_20 {
 
     // "solidityLikeConstructor" This is a solidity-like constructor. This method will only run once when the contract is deployed.
     public override onDeployment(_calldata: Calldata): void {
-        const maxSupply: u256 = u256.fromString('1000000000000000000000000000'); // Your max supply. (Here, 1 billion tokens)
-        const decimals: u8 = 18; // Your decimals.
-        const name: string = 'Test'; // Your token name.
-        const symbol: string = 'TEST'; // Your token symbol.
+        const maxSupply: u256 = u128.fromString('1000000000000000000000000').toU256(); // 1,000,000 tokens with 18 decimals
+        const decimals: u8 = 18; // 18
+        const name: string = 'YourTokenName'; // XtraBest
+        const symbol: string = 'SYM'; // XTB
 
         this.instantiate(new OP20InitParameters(maxSupply, decimals, name, symbol));
 
